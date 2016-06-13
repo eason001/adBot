7da1cc94c622d706bed35e3aa343209edc8080c1
yircheng <yi.cheng@digitaslbi.com>
2016-06-13 20:12:57 +0000

    pp done

--- ibot.py
+++ ibot.py
@@ -1,12 +1,12 @@
 import sys
 import pp
+import time
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from pyvirtualdisplay import Display
 
-
-
-def main(n, root):
+		
+def main(n,l,root,display):
     from selenium import webdriver
     from selenium.common.exceptions import TimeoutException
     from pyvirtualdisplay import Display
@@ -15,22 +15,21 @@ def main(n, root):
     src_path = root + '/src/'
     img_ext = '.png'
     src_ext = '.txt'
-    timeout = 30 #30 secs for timeout
     file = open(url_path,'r')
-
-    display = Display(visible=0, size=(1024, 768))
-    display.start()
+    timeout = 30 #30 secs for timeout
     driver = webdriver.Firefox()
     driver.maximize_window()
     driver.set_page_load_timeout(timeout)
 
-    for i, line in enumerate(file):
-    #while True:
-        #url = file.readline().strip()
-        if line == '' or i == n:
+    i=0
+    while True:
+        url = file.readline().strip()
+        if url == '' or i == (l+1)*n:
             break
-        if i < n:
+        if i < l*n:
+	    i += 1
 	    continue
+        i += 1
         domain = url.split('.')[1]
 	print domain + "..."
         try:
@@ -46,33 +45,39 @@ def main(n, root):
 		print "Ops, timeout occurred loading " + domain
 		continue
 
-    driver.close()
-    display.stop()
     file.close()
+    driver.close()
+
     print "All jobs DONE~!"
 
 if __name__=="__main__":
 	root = sys.argv[1]
 	n_cores = int(sys.argv[2])
+
+        display = Display(visible=0, size=(1024, 768))
+        display.start()
+
         ppservers = ()
 	job_server = pp.Server(n_cores, ppservers=ppservers)
         print "Starting pp with", job_server.get_ncpus(), "workers"	
 	#job1 = job_server.submit(main, (n_cores,))
         #job1()        
 	try:
-       		with open(root + 'urls.txt') as f:
+       		with open(root + '/urls.txt') as f:
             		t_lines =  sum(1 for _ in f)
     	except IOError:
         	print "Error opening file: please check your path and permission."
 	n_lines = t_lines / n_cores
-        for i in range(n_cores+1):
-		lines[i] = n_lines * (i+1)
+        lines = []
+	for i in range(n_cores+1):
+		lines.append(i)
 	start_time = time.time()
-	#inputs = (100000, 100100, 100200, 100300, 100400, 100500, 100600, 100700)
-	jobs = [(line, job_server.submit(main,(line,root))) for line in lines]
+	jobs = [(line, job_server.submit(main,(n_lines,line,root,display))) for line in lines]
 	for line, job in jobs:
-    		print "Job " + line + "..."
+    		print "Job " + str(line) + "..."
 		print job()
 
 	print "Time elapsed: ", time.time() - start_time, "s"
 	job_server.print_stats()
+
+	display.stop()
