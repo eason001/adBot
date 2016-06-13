151ca15dac3f52a647f92b4734b104cf804ef8ee
yircheng <yi.cheng@digitaslbi.com>
2016-06-13 14:11:03 +0000

    handle exception

--- ibot.py
+++ ibot.py
@@ -1,10 +1,6 @@
 import sys
-import os
-import time
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from pyvirtualdisplay import Display
 
 
@@ -13,26 +9,40 @@ def main():
     url_path = '/home/ubuntu/yi-ad-proj/urls.txt'
     img_path = '/home/ubuntu/yi-ad-proj/img/'
     src_path = '/home/ubuntu/yi-ad-proj/src/'
+    img_ext = '.png'
+    src_ext = '.txt'
+    timeout = 30 #30 secs for timeout
     file = open(url_path,'r')
+
     display = Display(visible=0, size=(1024, 768))
     display.start()
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.set_page_load_timeout(timeout)
 
     while True:
         url = file.readline().strip()
         if url == '':
             break
         domain = url.split('.')[1]
-        driver = webdriver.Firefox()
-        driver.maximize_window()
-        driver.get(url)
-        driver.get_screenshot_as_file(img_path + domain + '.png')
-        html_source = driver.page_source
-	f = open(src_path + domain + '.txt','w')
-	f.write(html_source.encode('utf-8'))
-	f.close()
-        #wait = WebDriverWait(driver, 3600)  # 3600s
-        driver.close()
+	print domain + "..."
+        try:
+		driver.get(url)
+        	driver.get_screenshot_as_file(img_path + domain + img_ext)
+		print img_path + domain + img_ext + " saved successfully!"
+        	html_source = driver.page_source
+		f = open(src_path + domain + src_ext,'w')
+		f.write(html_source.encode('utf-8'))
+		f.close()
+		print src_path + domain + src_ext + " saved successfully!"
+	except TimeoutException:
+		print "Ops, timeout occurred loading " + domain
+		continue
+
+    driver.close()
     display.stop()
+    file.close()
+    print "All jobs DONE~!"
 
 if __name__=="__main__":
 	main()
